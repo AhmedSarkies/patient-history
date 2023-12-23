349cd77fdbab658835908820e2b68ee5f63960fb
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-23 05:16:31 +0200

    Add Loading Component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Col, Container, Row } from "reactstrap";
+
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Loading;
