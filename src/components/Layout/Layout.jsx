4eed9d9a086b79a1d9f0b59a5022401327bc8a1b
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-23 05:15:30 +0200

    Add Layout Component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import Routers from "../../routes/Routers.js";
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <Routers />
+    </Fragment>
+  );
+};
+
+export default Layout;
