e77833a43ca3c3c759c07963f35bd94d554d32dc
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-24 20:53:30 +0200

    update Patient Page

--- src/pages/Patient.jsx
+++ src/pages/Patient.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+
+import { Col, Container, Row } from "reactstrap";
+
+import { Helmet, Navbar } from "../components";
 
 const Patient = () => {
   return (
-    <div>Patient</div>
-  )
-}
+    <Helmet title="">
+      <section className="patient">
+        <Container
+          style={{
+            maxWidth: "100%",
+            paddingLeft: "0px",
+          }}
+        >
+          <Row>
+            <Col xl="3">
+              <Navbar />
+            </Col>
+            <Col xl="9">
+              <div className="patient__content"></div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
 
-export default Patient
+export default Patient;
