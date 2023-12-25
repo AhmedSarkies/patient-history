63fc831e40c2667a2b357a7573889aed22a96a5f
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-25 12:30:10 +0200

    Update index.js in pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as Patient } from "./Patient";
+export { default as PersonalInformation } from "./PersonalInformation";
+export { default as GeneralExamination } from "./GeneralExamination";
+export { default as GynecologicalHistory } from "./GynecologicalHistory";
+export { default as ObstetricsHistory } from "./ObstetricsHistory";
+export { default as CervicalCancer } from "./CervicalCancer";
+export { default as BreastCancer } from "./BreastCancer";
+export { default as OvarianCancer } from "./OvarianCancer";
+export { default as UterineCancer } from "./UterineCancer";
+export { default as Osteoporosis } from "./Osteoporosis";
+export { default as PreEclampsia } from "./PreEclampsia";
