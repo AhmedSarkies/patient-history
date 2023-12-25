5dd9adf42da8eaf6f036b7437302497dd06e70a0
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-25 21:53:12 +0200

    Update index.js in components folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Loading } from "./Loading/Loading";
 export { default as Layout } from "./Layout/Layout";
 export { default as Helmet } from "./Helmet/Helmet";
 export { default as Navbar } from "./Navbar/Navbar";
+export { default as PersonalInformationComponent     } from "./UI/PersonalInformationComponent/PersonalInformationComponent";
