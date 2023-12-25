7eaf7b83ed572cc01b185119ef1cd480e98b78b9
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-25 12:26:46 +0200

    Initialize CervicalCancer Page

---
+++ src/pages/CervicalCancer.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+
+const CervicalCancer = () => {
+  return (
+    <Fragment>
+      <h1>CervicalCancer</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th className=""></th>
+            <th className="border">2</th>
+            <th className="border">3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border">1</td>
+            <td className="border">1</td>
+            <td className="border">1</td>
+          </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default CervicalCancer;
