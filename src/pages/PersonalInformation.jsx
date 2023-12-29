6c60890d0004d7274d9202aa6cf7d7be856d2e5c
Sarkies <Ahmed.Sarkes@gmail.com>
2023-12-29 21:40:00 +0200

    update PersonalInformation Page

--- src/pages/PersonalInformation.jsx
+++ src/pages/PersonalInformation.jsx
@@ -44,7 +44,7 @@ const PersonalInformation = () => {
       <section className="personal__information">
         <Container style={{ maxWidth: "100%" }}>
           <Row style={{ paddingBottom: "2.5rem" }}>
-            <Col lg="12" className="pe-3">
+            <Col lg="12" className="pe-0 pe-sm-3 pe-xs-4">
               <PersonalInformationComponent logout={false} />
             </Col>
             <Col lg="12" className="mt-3 mb-2">
