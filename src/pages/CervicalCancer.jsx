25a4fcc5acb7e31aff22040ef748be440c11f1cf
Sarkies <Ahmed.Sarkes@gmail.com>
2024-01-31 00:49:44 +0200

    update CervicalCancer.jsx

--- src/pages/CervicalCancer.jsx
+++ src/pages/CervicalCancer.jsx
@@ -57,42 +57,48 @@ const CervicalCancer = () => {
                       <Col md="4" className="pe-0 pe-lg-3">
                         <div className="form__group">
                           <label className="form__label">HPV Vaccine</label>
-                          <div className="form__radio-group">
-                            <div className="form__radio d-flex justify-content-start align-items-center gap-2">
-                              <input
-                                className="form__input"
-                                type="radio"
-                                placeholder="Enter your HPV Vaccine"
-                                id="yes-hpv-vaccine"
-                                name="hpv-vaccine"
-                                value={hpvVaccine}
-                                onChange={(e) => setHpvVaccine(true)}
-                              />
-                              <label
-                                className="form__radio-label"
-                                htmlFor="yes-hpv-vaccine"
-                              >
-                                Yes
-                              </label>
+                          {true ? (
+                            <p className="form-editing-field table__input mb-0">
+                              Yes
+                            </p>
+                          ) : (
+                            <div className="form__radio-group">
+                              <div className="form__radio d-flex justify-content-start align-items-center gap-2">
+                                <input
+                                  className="form__input"
+                                  type="radio"
+                                  placeholder="Enter your HPV Vaccine"
+                                  id="yes-hpv-vaccine"
+                                  name="hpv-vaccine"
+                                  value={hpvVaccine}
+                                  onChange={(e) => setHpvVaccine(true)}
+                                />
+                                <label
+                                  className="form__radio-label"
+                                  htmlFor="yes-hpv-vaccine"
+                                >
+                                  Yes
+                                </label>
+                              </div>
+                              <div className="form__radio d-flex justify-content-start align-items-center gap-2">
+                                <input
+                                  className="form__input"
+                                  type="radio"
+                                  placeholder="Enter your HPV Vaccine"
+                                  id="no-hpv-vaccine"
+                                  name="hpv-vaccine"
+                                  value={hpvVaccine}
+                                  onChange={(e) => setHpvVaccine(false)}
+                                />
+                                <label
+                                  className="form__radio-label"
+                                  htmlFor="no-hpv-vaccine"
+                                >
+                                  No
+                                </label>
+                              </div>
                             </div>
-                            <div className="form__radio d-flex justify-content-start align-items-center gap-2">
-                              <input
-                                className="form__input"
-                                type="radio"
-                                placeholder="Enter your HPV Vaccine"
-                                id="no-hpv-vaccine"
-                                name="hpv-vaccine"
-                                value={hpvVaccine}
-                                onChange={(e) => setHpvVaccine(false)}
-                              />
-                              <label
-                                className="form__radio-label"
-                                htmlFor="no-hpv-vaccine"
-                              >
-                                No
-                              </label>
-                            </div>
-                          </div>
+                          )}
                         </div>
                       </Col>
                     </Col>
@@ -118,26 +124,38 @@ const CervicalCancer = () => {
                                     <span>VIA Test</span>
                                   </td>
                                   <td className="cell__input">
-                                    <input
-                                      className="form__input table__input"
-                                      type="text"
-                                      id="result-via-test"
-                                      value={resultViaTest}
-                                      onChange={(e) =>
-                                        setResultViaTest(e.target.value)
-                                      }
-                                    />
+                                    {true ? (
+                                      <p className="form-editing-field table__input mb-0">
+                                        26-39
+                                      </p>
+                                    ) : (
+                                      <input
+                                        className="form__input table__input"
+                                        type="text"
+                                        id="result-via-test"
+                                        value={resultViaTest}
+                                        onChange={(e) =>
+                                          setResultViaTest(e.target.value)
+                                        }
+                                      />
+                                    )}
                                   </td>
                                   <td className="cell__input">
-                                    <input
-                                      className="form__input table__input"
-                                      type="text"
-                                      id="comments-via-test"
-                                      value={commentsViaTest}
-                                      onChange={(e) =>
-                                        setCommentsViaTest(e.target.value)
-                                      }
-                                    />
+                                    {true ? (
+                                      <p className="form-editing-field table__input mb-0">
+                                        26-39
+                                      </p>
+                                    ) : (
+                                      <input
+                                        className="form__input table__input"
+                                        type="text"
+                                        id="comments-via-test"
+                                        value={commentsViaTest}
+                                        onChange={(e) =>
+                                          setCommentsViaTest(e.target.value)
+                                        }
+                                      />
+                                    )}
                                   </td>
                                 </tr>
                               </tbody>
@@ -162,26 +180,38 @@ const CervicalCancer = () => {
                                     <span>Pap smear</span>
                                   </td>
                                   <td className="cell__input">
-                                    <input
-                                      className="form__input table__input"
-                                      type="text"
-                                      id="result-pap-smear"
-                                      value={resultPapSmear}
-                                      onChange={(e) =>
-                                        setResultPapSmear(e.target.value)
-                                      }
-                                    />
+                                    {true ? (
+                                      <p className="form-editing-field table__input mb-0">
+                                        26-39
+                                      </p>
+                                    ) : (
+                                      <input
+                                        className="form__input table__input"
+                                        type="text"
+                                        id="result-pap-smear"
+                                        value={resultPapSmear}
+                                        onChange={(e) =>
+                                          setResultPapSmear(e.target.value)
+                                        }
+                                      />
+                                    )}
                                   </td>
                                   <td className="cell__input">
-                                    <input
-                                      className="form__input table__input"
-                                      type="text"
-                                      id="comments-pap-smear"
-                                      value={commentsPapSmear}
-                                      onChange={(e) =>
-                                        setCommentsPapSmear(e.target.value)
-                                      }
-                                    />
+                                    {true ? (
+                                      <p className="form-editing-field table__input mb-0">
+                                        26-39
+                                      </p>
+                                    ) : (
+                                      <input
+                                        className="form__input table__input"
+                                        type="text"
+                                        id="comments-pap-smear"
+                                        value={commentsPapSmear}
+                                        onChange={(e) =>
+                                          setCommentsPapSmear(e.target.value)
+                                        }
+                                      />
+                                    )}
                                   </td>
                                 </tr>
                               </tbody>
@@ -196,20 +226,28 @@ const CervicalCancer = () => {
                             >
                               Recommendations
                             </label>
-                            <textarea
-                              className="form__input col-12 col-lg-12 col-md-8"
-                              placeholder="Enter your recommendations"
-                              id="recommendations"
-                              style={{
-                                minHeight: "215px",
-                                maxHeight: "215px",
-                                resize: "none",
-                              }}
-                              value={recommendations}
-                              onChange={(e) =>
-                                setRecommendations(e.target.value)
-                              }
-                            ></textarea>
+                            {true ? (
+                              <p className="form-editing-field form-editing-field-textarea">
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has been
+                                the industry's standard
+                              </p>
+                            ) : (
+                              <textarea
+                                className="form__input col-12 col-lg-12 col-md-8"
+                                placeholder="Enter your recommendations"
+                                id="recommendations"
+                                style={{
+                                  minHeight: "215px",
+                                  maxHeight: "215px",
+                                  resize: "none",
+                                }}
+                                value={recommendations}
+                                onChange={(e) =>
+                                  setRecommendations(e.target.value)
+                                }
+                              ></textarea>
+                            )}
                           </div>
                         </Col>
                       </Row>
@@ -260,14 +298,60 @@ const CervicalCancer = () => {
                     </Col>
                     <Col
                       lg="12"
-                      className="col__submit d-flex justify-content-end align-items-center mt-5"
+                      className="col__submit d-flex justify-content-end align-items-center gap-sm-4 gap-3 mt-5"
                     >
-                      <button
-                        type="submit"
-                        className="col-lg-2 col-sm-3 col-4 save__btn me-1"
-                      >
-                        Save
-                      </button>
+                      {true ? (
+                        <Fragment>
+                          <button
+                            type="button"
+                            className="col-lg-2 col-4 edit__btn d-flex justify-content-center align-items-center gap-sm-2 gap-1"
+                          >
+                            <span>
+                              <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M3 21V16.75L16.2 3.575C16.4 3.39167 16.621 3.25 16.863 3.15C17.105 3.05 17.359 3 17.625 3C17.8917 3 18.15 3.05 18.4 3.15C18.65 3.25 18.8667 3.4 19.05 3.6L20.425 5C20.625 5.18333 20.771 5.4 20.863 5.65C20.955 5.9 21.0007 6.15 21 6.4C21 6.66667 20.954 6.921 20.862 7.163C20.77 7.405 20.6243 7.62567 20.425 7.825L7.25 21H3ZM17.6 7.8L19 6.4L17.6 5L16.2 6.4L17.6 7.8Z"
+                                  fill="white"
+                                />
+                              </svg>
+                            </span>
+                            Edit
+                          </button>
+                          <button
+                            type="button"
+                            className="col-lg-3 col-sm-5 col-8 download__btn save__btn d-flex justify-content-center align-items-center gap-2"
+                            onClick={() => window.print()}
+                          >
+                            <span>
+                              <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  d="M12 16L7 11L8.4 9.55L11 12.15V4H13V12.15L15.6 9.55L17 11L12 16ZM6 20C5.45 20 4.979 19.804 4.587 19.412C4.195 19.02 3.99934 18.5493 4 18V15H6V18H18V15H20V18C20 18.55 19.804 19.021 19.412 19.413C19.02 19.805 18.5493 20.0007 18 20H6Z"
+                                  fill="white"
+                                />
+                              </svg>
+                            </span>
+                            Download a copy
+                          </button>
+                        </Fragment>
+                      ) : (
+                        <button
+                          type="submit"
+                          className="col-lg-2 col-sm-3 col-4 save__btn me-1"
+                        >
+                          Save
+                        </button>
+                      )}
                     </Col>
                   </Row>
                 </form>
