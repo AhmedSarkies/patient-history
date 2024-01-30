385ece9e8b50762e43a37169c91ed0d48b43249e
Sarkies <Ahmed.Sarkes@gmail.com>
2024-01-31 00:45:22 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import "./App.css";
 const App = () => {
   return (
     <div className="App">
+      <div id="printContent">
+        <span className="patient-name m-0">Patient’s name</span>
+        <span className="patient-id m-0">485967</span>
+      </div>
       <Layout />
     </div>
   );
